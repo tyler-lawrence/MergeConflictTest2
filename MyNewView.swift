efb82ac038ac11871bb91db8cfb85560ee336f05
Tyler Lawrence <tyler.lawrence15@gmail.com>
2023-03-09 14:54:38 -0500

    my new view goodbye

---
+++ MyNewView.swift
@@ -0,0 +1,21 @@
+//
+//  MyNewView.swift
+//  MergeConflictTest
+//
+//  Created by Tyler Lawrence1 on 3/9/23.
+//
+
+import SwiftUI
+
+struct MyNewView: View {
+    var body: some View {
+        Text("Goodbye, World!")
+            .foregroundColor(.red)
+    }
+}
+
+struct MyNewView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyNewView()
+    }
+}
