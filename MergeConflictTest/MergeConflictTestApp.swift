6d3aef6890e4e3a3c21b110b11cfcd83ef08c01e
Tyler Lawrence <tyler.lawrence15@gmail.com>
2023-03-09 14:48:18 -0500

    Initial Commit

---
+++ MergeConflictTest/MergeConflictTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MergeConflictTestApp.swift
+//  MergeConflictTest
+//
+//  Created by Tyler Lawrence1 on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MergeConflictTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
