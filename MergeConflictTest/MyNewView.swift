c3f658382c8b8752352f5259900bae9dded85cf4
Tyler Lawrence <tyler.lawrence15@gmail.com>
2023-03-09 14:53:58 -0500

    added my new view

---
+++ MergeConflictTest/MyNewView.swift
@@ -0,0 +1,21 @@
+//
+//  MyNewView.swift
+//  MergeConflictTest
+//
+//  Created by Tyler Lawrence1 on 3/9/23.
+//
+
+import SwiftUI
+
+struct MyNewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.blue)
+    }
+}
+
+struct MyNewView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyNewView()
+    }
+}
